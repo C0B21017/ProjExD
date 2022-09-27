c1ab8de617bb9d8941d1594c400317a90973392c
Izumi <c0b210172b@edu.teu.ac.jp>
2022-09-27 16:59:49 +0900

    問題と解答終了

--- ex01/alphabet.py
+++ ex01/alphabet.py
@@ -3,31 +3,35 @@ alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 alphabet=list(alphabet)
 choice_num=8
 loss_num=3
+show_num=choice_num-loss_num
 chalenge=3
 
 def mondai():
-    choiced_num=" ".join(random.sample(alphabet,choice_num))
-    choiced_num2=" ".join(random.sample(choiced_num,choice_num-loss_num))
-    print("対象文字:"+"\n"+choiced_num)
-    print("表示文字:"+"\n"+choiced_num2)
+    choiced_num=random.sample(alphabet,choice_num)
+    choiced_num2=random.sample(choiced_num,show_num)
+    print("対象文字:"+"\n"+" ".join(choiced_num))
+    print("表示文字:"+"\n"+" ".join(choiced_num2))
     t_choiced=set(choiced_num)
     t_choiced2=set(choiced_num2)
     return  (t_choiced)-(t_choiced2)
 
 def kaitou(d):
-    ans=print(input("欠損文字はいくつありますか？"))
+    ans=int(input("欠損文字はいくつありますか？"))
+
     if ans == len(d):
         for i in range(chalenge):
             a=input(f"{i+1}文字目の文字を入力してください？")
             if a in d:
-                d=d-a
+                d.remove(a)
                 if len(d)==0:
                     print("おめでとう！正解だよ")
             else:
                 print("失敗！またチャレンジしてね")
 
-loss=mondai()
+print(loss)
 kaitou(loss)
+
+#時間計測つける
             
 
             
