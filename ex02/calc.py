f9e12658c7991b653a5384f159b6e82d1b8ff3b2
Izumi <c0b210172b@edu.teu.ac.jp>
2022-10-04 16:45:38 +0900

    作成中

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,12 +1,11 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
 
-
 root = tk.Tk()
 root.title("電卓")
 root.geometry("300x500")
 
-def button_click(event):
+def click_button(event):
     btn = event.widget
     txt = btn["text"]
     entry.insert(tk.END, txt)
@@ -21,16 +20,25 @@ def click_equal(event):
 entry = tk.Entry(root, width=10, font=("Times New Roman",40), justify="right")
 entry.grid(row=0, column=0, columnspan=3)
 
-for i,num in enumerate(range(9,-3,-1),0):
-    if num==-1:
-        num="+"
-    if num==-2:
-        num="="
-    btn = tk.Button(root,text=num,font=("Times New Roman",30),width=4,height=1,)
-    if num =="=":
-        btn.bind("<1>",click_equal)
-    else:
-        btn.bind("<1>",button_click)
-    btn.grid(row=(i//3)+1, column=i%3)
+y, x = 1, 0 
+numbers = list(range(9, -1, -1)) 
+operators = ["+"] 
+for i, num in enumerate(numbers+operators, 1):
+    btn = tk.Button(root, text=f"{num}", font=("", 30), width=4, height=1)
+    btn.bind("<1>", click_button)
+    btn.grid(row=y, column=x)
+    x += 1
+    if i%3 == 0:
+        y += 1
+        x = 0
 
+btn = tk.Button(root, text=f"=", font=("", 30), width=4, height=1)
+btn.bind("<1>", click_equal)
+btn.grid(row=y, column=x)
+y+=1
+x+=1
+
+btn = tk.Button(root, text="C", font=("", 30), width=4, height=1)
+btn.bind("<1>",entry.delete(tk.END))
+btn.grid(row=y,column=x)
 root.mainloop()
