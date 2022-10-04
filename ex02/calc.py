063db4e64e7dac4d25be7e9f778ec4c2b3744c03
Izumi <c0b210172b@edu.teu.ac.jp>
2022-10-04 15:03:09 +0900

    練習4

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
 
+
 root = tk.Tk()
 root.title("電卓")
 root.geometry("300x500")
@@ -9,8 +10,13 @@ def button_click(event):
     btn = event.widget
     txt = btn["text"]
     tkm.showinfo(txt, f"{txt}ボタンが押されました")
-for num in range(9,-1,-1):
-    btn = tk.Button(root,text=num,font=("Times New Roman",30),width=4,height=2)
-    btn.pack()
+
+entry = tk.Entry(root, width=10, font=("Times New Roman",40), justify="right")
+entry.grid(row=0, column=0, columnspan=3)
+
+for i,num in enumerate(range(9,-1,-1),0):
+    btn = tk.Button(root,text=num,font=("Times New Roman",30),width=4,height=2,)
+    btn.bind("<1>",button_click)
+    btn.grid(row=(i//3)+1, column=i%3)
 
 root.mainloop()
