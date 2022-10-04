499afbd3441da9d3514d3221f917ff0ad475a016
Izumi <c0b210172b@edu.teu.ac.jp>
2022-10-04 18:21:47 +0900

    コメント対応しました#5

--- ex02/calc.py
+++ ex02/calc.py
@@ -16,21 +16,49 @@ def click_equal(event):
     res = eval(eqn)
     entry.delete(0,tk.END)
     entry.insert(tk.END,res)
+
+def make_grid(x,y):
+    btn.grid(row=y, column=x)
+    x += 1
+    if x%3 == 0:
+        y += 1
+        x  = 0
     
 
 entry = tk.Entry(root, width=10, font=("Times New Roman",40), justify="right")
 entry.grid(row=0, column=0, columnspan=3)
 
-for i,num in enumerate(range(9,-3,-1),0):
-    if num==-1:
-        num="+"
-    if num==-2:
-        num="="
-    btn = tk.Button(root,text=num,font=("Times New Roman",30),width=4,height=1,)
-    if num =="=":
-        btn.bind("<1>",click_equal)
-    else:
-        btn.bind("<1>",button_click)
-    btn.grid(row=(i//3)+1, column=i%3)
+nums=list(range(9,-1,-1))  #コメント#5
+operation=["=","+","-"]    #コメント#5
 
+x, y = 0, 1     #９の電卓初期位置
+
+for num in (nums): #数字並べ
+    btn = tk.Button(root,text=num,font=("Times New Roman",30),width=4,height=1,)
+    btn.bind("<1>",button_click)
+    btn.grid(row=y, column=x)
+    x += 1
+    if x%3 == 0:
+        y += 1
+        x  = 0
+
+for ope in (operation):
+    if  ope == "=":
+        btn = tk.Button(root,text=ope,font=("Times New Roman",30),width=4,height=1,)
+        btn.bind("<1>",click_equal)
+        btn.grid(row=y, column=x)
+        x += 1
+        if x%3 == 0:
+            y += 1
+            x  = 0
+    else:
+        btn = tk.Button(root,text=ope,font=("Times New Roman",30),width=4,height=1,)
+        btn.bind("<1>",button_click)
+        btn.grid(row=y, column=x)
+        x += 1
+        if x%3 == 0:
+            y += 1
+            x  = 0
 root.mainloop()
+
+#コメント対応しました　#5
