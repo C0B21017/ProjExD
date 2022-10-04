47097c35b73db2d305c3bb9f85e27531081454bd
Izumi <c0b210172b@edu.teu.ac.jp>
2022-10-04 15:28:39 +0900

    練習7

--- ex02/calc.py
+++ ex02/calc.py
@@ -11,14 +11,26 @@ def button_click(event):
     txt = btn["text"]
     entry.insert(tk.END, txt)
 
+def click_equal(event):
+    eqn = entry.get()
+    res = eval(eqn)
+    entry.delete(0,tk.END)
+    entry.insert(tk.END,res)
+    
+
 entry = tk.Entry(root, width=10, font=("Times New Roman",40), justify="right")
 entry.grid(row=0, column=0, columnspan=3)
 
-for i,num in enumerate(range(9,-2,-1),0):
+for i,num in enumerate(range(9,-3,-1),0):
     if num==-1:
         num="+"
+    if num==-2:
+        num="="
     btn = tk.Button(root,text=num,font=("Times New Roman",30),width=4,height=1,)
-    btn.bind("<1>",button_click)
+    if num =="=":
+        btn.bind("<1>",click_equal)
+    else:
+        btn.bind("<1>",button_click)
     btn.grid(row=(i//3)+1, column=i%3)
 
 root.mainloop()
