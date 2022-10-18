a2b2682a79656d1577b3099d76b65aaba4745456
Izumi <c0b210172b@edu.teu.ac.jp>
2022-10-18 17:02:28 +0900

    完成版

--- ex03/maze.py
+++ ex03/maze.py
@@ -13,23 +13,48 @@ def key_up(event):
     key = ""
     print(key)
 
+
 def main_proc():
-    global cx, cy
+    global mx, my,flag
+    
     if key=="w":
-        cy -= 20
+        my -= 1
     
     if key=="s":
-        cy += 20
+        my += 1
     
     if key=="a":
-        cx -= 20
+        mx -= 1
     
     if key=="d":
-        cx += 20
+        mx += 1
+
+    if maze[my][mx]!=1:
+        pass
+    else:
+        if key=="w" or key=="UP":
+            my += 1
+        
+        if key=="s" or key=="DOWN":
+            my -= 1
+        
+        if key=="a" or key=="LEFT":
+            mx += 1
+        
+        if key=="d" or key=="RIGHT":
+            mx -= 1
+
+
+    cx,cy = mx*100+50, my*100+50
     canvas.coords("tori",cx,cy)
+    if flag == 0:
+        if maze[my][mx]==2:  #Goal
+            tkm.showinfo("ははは","ゴールおめでとう！！！")
+            flag = 1
     root.after(100,main_proc)
 
 if __name__=="__main__":
+    flag=0
     root = tk.Tk()
     root.title("迷えるこうかとん")#練習1
 
@@ -37,17 +62,24 @@ if __name__=="__main__":
     canvas=can(root, width=1500, height=900, bg="black")
 
     maze=mm.make_maze(15,9)
+    
     mm.show_maze(canvas,maze)
+    maze[-2][-2]=2
+    canvas.create_rectangle(100,100,200,200, fill="cyan")
+    canvas.create_rectangle(1300,700,1400,800, fill="yellow")
 
-    cx, cy = 300,400 #練習3
+    # cx, cy = 150,150 #練習3
+    mx, my = 1,1
+    cx, cy = mx*100+50, my*100+50
     tori =tk.PhotoImage(file="fig/9.png")
+    # canvas.create_image(cx,cy,image=tori,tag="tori")
     canvas.create_image(cx,cy,image=tori,tag="tori")
-
     key = ""
     root.bind("<KeyPress>",key_down)
     root.bind("<KeyRelease>",key_up)
 
     main_proc()
 
-    canvas.pack()
+    canvas.pack() 
+
     root.mainloop()
