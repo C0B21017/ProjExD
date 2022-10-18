06c942206130967f3092e932c734e4a04bff6b8a
Izumi <c0b210172b@edu.teu.ac.jp>
2022-10-18 14:50:13 +0900

    練習8&9

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
+import maze_maker as mm
 
 def key_down(event):
     global key
@@ -44,6 +45,7 @@ if __name__=="__main__":
     root.bind("<KeyRelease>",key_up)
 
     main_proc()
+    mm.make_maze(15,9)
 
     canvas.pack()
     root.mainloop()
