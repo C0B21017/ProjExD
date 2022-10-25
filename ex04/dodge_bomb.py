913db80372d64a5a933e2053a9964c80c38da8ab
Izumi <c0b210172b@edu.teu.ac.jp>
2022-10-25 14:34:37 +0900

    練習3

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -3,19 +3,29 @@ import sys
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
-    scrn_sfc = pg.display.set_mode((1200,600))
+    scrn_sfc = pg.display.set_mode((1200,800))
     
     bg_sfc = pg.image.load("ex04/pg_bg.jpg")
     bg_rct = bg_sfc.get_rect() #練習1
+
+    #練習3
+    tori_sfc = pg.image.load("ex04/fig/6.png")
+    tori_sfc = pg.transform.rotozoom(tori_sfc, 0, 2.0)
+    tori_rct = tori_sfc.get_rect()
+    tori_rct = 900,400
+    
+
     clock = pg.time.Clock() #タイムインスタンス作成
     while True:
         scrn_sfc.blit(bg_sfc,bg_rct) #練習2
-        pg.display.update()
+        
         clock.tick(1000)
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return
 
+        scrn_sfc.blit(tori_sfc,tori_rct)
+        pg.display.update()
 
 if __name__=="__main__":
     pg.init()
